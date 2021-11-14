a0f6a1169f43ebb8c24dc819201b58e314ec425f
MikhailMaslo <maslomihail@gmail.com>
2021-11-14 17:23:03 +0400

    Introduce PullBar

---
+++ BottomSheet/Core/BottomSheetNavigationPullBar.swift
@@ -0,0 +1,47 @@
+//
+//  BottomSheetNavigationPullBar.swift
+//  BottomSheet
+//
+//  Created by Mikhail Maslo on 14.11.2021.
+//  Copyright © 2021 Joom. All rights reserved.
+//
+
+import UIKit
+
+extension BottomSheetPresentationController {
+    final class PullBar: UIView {
+        enum Style {
+            static let size = CGSize(width: 40, height: 4)
+        }
+
+        private let centerView: UIView = {
+            let view = UIView()
+            view.frame.size = Style.size
+            view.backgroundColor = UIColor.white.withAlphaComponent(0.6)
+            view.layer.cornerRadius = Style.size.height * 0.5
+            return view
+        }()
+
+        init() {
+            super.init(frame: .zero)
+
+            backgroundColor = .clear
+
+            setupSubviews()
+        }
+
+        required init?(coder: NSCoder) {
+            preconditionFailure("init(coder:) has not been implemented")
+        }
+
+        private func setupSubviews() {
+            addSubview(centerView)
+        }
+
+        override func layoutSubviews() {
+            super.layoutSubviews()
+
+            centerView.center = bounds.center
+        }
+    }
+}
---
+++ BottomSheet/Core/BottomSheetPresentationController.swift
@@ -0,0 +1,13 @@
+//
+//  BottomSheetPresentationController.swift
+//  BottomSheet
+//
+//  Created by Mikhail Maslo on 14.11.2021.
+//  Copyright © 2021 Joom. All rights reserved.
+//
+
+import UIKit
+
+public final class BottomSheetPresentationController: UIPresentationController {
+    
+}
