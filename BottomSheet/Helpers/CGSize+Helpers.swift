cb2a5c9363f2bec492f6e02b03e002305e02e890
MikhailMaslo <maslomihail@gmail.com>
2021-11-14 17:04:22 +0400

    Introduce BottomSheet lib

---
+++ BottomSheet/Helpers/CGSize+Helpers.swift
@@ -0,0 +1,15 @@
+//
+//  CGSize+Helpers.swift
+//  BottomSheetDemo
+//
+//  Created by Mikhail Maslo on 08.11.2021.
+//  Copyright © 2021 Joom. All rights reserved.
+//
+
+import UIKit
+
+extension CGSize {
+    init(uniform: Double) {
+        self.init(width: uniform, height: uniform)
+    }
+}
