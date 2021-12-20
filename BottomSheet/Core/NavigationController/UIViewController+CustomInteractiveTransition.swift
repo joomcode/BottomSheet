dc433cb8af32b4c6314cc4ac964c83d227711eba
MikhailMaslo <maslomihail@gmail.com>
2021-12-20 23:06:29 +0300

    Add NavigationController group and move presentation controller related code to presentation group

---
+++ BottomSheet/Core/NavigationController/UIViewController+CustomInteractiveTransition.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewController+CustomInteractiveTransition.swift
+//  BottomSheet
+//
+//  Created by Mikhail Maslo on 20.12.2021.
+//  Copyright © 2021 Joom. All rights reserved.
+//
+
+import UIKit
