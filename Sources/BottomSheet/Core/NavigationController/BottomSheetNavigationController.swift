b85058240dfe806d7897edb422d92d36c9bd99d4
Tomasz Wagner <61847268+luka-w@users.noreply.github.com>
2023-07-01 13:24:51 +0200

    Update BottomSheetNavigationController.swift

--- Sources/BottomSheet/Core/NavigationController/BottomSheetNavigationController.swift
+++ Sources/BottomSheet/Core/NavigationController/BottomSheetNavigationController.swift
@@ -26,6 +26,10 @@ public final class BottomSheetNavigationController: UINavigationController {
         super.init(rootViewController: rootViewController)
     }
 
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+
     @available(*, unavailable)
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
