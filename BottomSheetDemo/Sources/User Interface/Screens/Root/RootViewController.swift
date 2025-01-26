e8b874f391b11f42db6d8ca686dbba7ac2006979
Mikhail Maslo <mikhailmaslo@joom.com>
2025-01-26 20:23:31 +0100

    Example without scroll view

--- BottomSheetDemo/Sources/User Interface/Screens/Root/RootViewController.swift
+++ BottomSheetDemo/Sources/User Interface/Screens/Root/RootViewController.swift
@@ -51,17 +51,85 @@ final class RootViewController: UIViewController {
 
     @objc
     private func handleShowBottomSheet() {
-        let viewController = ResizeViewController(initialHeight: 300)
-        presentBottomSheetInsideNavigationController(
-            viewController: viewController,
-            configuration: .default,
-            canBeDismissed: {
-                // return `true` or `false` based on your business logic
-                true
-            },
-            dismissCompletion: {
-                // handle bottom sheet dismissal completion
-            }
-        )
+        showBottomSheetWithoutScrollView()
+        /*
+         let viewController = ResizeViewController(initialHeight: 300)
+         presentBottomSheetInsideNavigationController(
+             viewController: viewController,
+             configuration: .default,
+             canBeDismissed: {
+                 // return `true` or `false` based on your business logic
+                 true
+             },
+             dismissCompletion: {
+                 // handle bottom sheet dismissal completion
+             }
+         )
+          */
+    }
+
+    private func showBottomSheetWithoutScrollView() {
+        let viewController = ViewControllerWithoutScrollView(nibName: nil, bundle: nil)
+        presentBottomSheet(viewController: viewController, configuration: .default)
+    }
+}
+
+final class ViewControllerWithoutScrollView: UIViewController {
+    private let contentView = UIView()
+    private lazy var label1: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.text = """
+        [Label 1]
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        """
+        return label
+    }()
+
+    private lazy var label2: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.text = """
+        [Label 2]
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        """
+        return label
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+
+        setupSubviews()
+    }
+
+    private func setupSubviews() {
+        view.addSubview(contentView)
+
+        contentView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.leading.trailing.equalToSuperview()
+        }
+
+        // put any subviews you want
+        // contentView size should be a sum of all sizes (via constraints / manually)
+        contentView.addSubview(label1)
+        contentView.addSubview(label2)
+
+        label1.snp.makeConstraints { make in
+            make.leading.top.trailing.equalToSuperview()
+        }
+
+        label2.snp.makeConstraints { make in
+            make.top.equalTo(label1.snp.bottom).offset(100)
+            make.leading.trailing.bottom.equalToSuperview()
+        }
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+
+        preferredContentSize = contentView.frame.size
     }
 }
